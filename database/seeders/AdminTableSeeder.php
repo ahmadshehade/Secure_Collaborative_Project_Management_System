d53f0cb4e9bdb8c081c9fe809e563d8457b3b233
Ahmad Shehade <std.ahmad.shehade@gmail.com>
2025-07-07 00:21:09 +0300

    updateAdminTableSeeder

--- database/seeders/AdminTableSeeder.php
+++ database/seeders/AdminTableSeeder.php
@@ -27,84 +27,10 @@ class AdminTableSeeder extends Seeder
         ]);
 
         $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => "api"]);
-        $userRole = Role::firstOrCreate(['name' => 'member', 'guard_name' => "api"]);
-        $managerRole = Role::firstOrCreate(['name' => 'project_manager', 'guard_name' => "api"]);
+        Role::firstOrCreate(['name' => 'member', 'guard_name' => "api"]);
+        Role::firstOrCreate(['name' => 'project_manager', 'guard_name' => "api"]);
 
         $admin->assignRole($adminRole);
-        $permissions = [
-
-            'create teams',
-            'update teams',
-            'delete teams',
-
-            'create projects',
-            'update projects',
-            'delete projects',
-
-
-            'create comments',
-            'update owner comments',
-            'delete comments',
-
-
-        ];
-
-        foreach ($permissions as $permission) {
-            $perm = Permission::firstOrCreate(['name' => $permission]);
-            $adminRole->givePermissionTo($perm);
-        }
-
-
-        $userPermissions = [
-            "create teams",
-            'update owner teams',
-            'delete owner teams',
-            'view owner teams',
-
-            'create projects', 
-            'update owner projects', 
-            'delete owner projects',
-
-            'create tasks'
-            , 'update own tasks'
-            , 'delete own tasks',
-
-            'create comments',
-            'update owner comments',
-            'delete comments',
-
-        ];
-
-        foreach ($userPermissions as $permission) {
-            $perm = Permission::firstOrCreate(['name' => $permission]);
-            $userRole->givePermissionTo($perm);
-        }
-
-
-        $managerPermissions = [
-            // Projects
-           'create projects',
-            'update projects',
-            'delete projects',
-
-            // Tasks
-            'create tasks',
-            'view tasks',
-            'update tasks',
-            'delete tasks',
-            
-
-            'create comments',
-            'update comments',
-            'delete comments',
-
-           
-        ];
-
-
-        foreach ($managerPermissions as $permission) {
-            $perm = Permission::firstOrCreate(['name' => $permission]);
-            $managerRole->givePermissionTo($perm);
-        }
+    
     }
 }
